90d9c744dee6db2aa7c3007c457e574ad87ada1f
AL-Milanov <alexandermilanov161195@gmail.com>
2022-11-07 12:01:36 +0200

    Add project files.

---
+++ CinemaHalls.Api/Controllers/ScreeningController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaHalls.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreeningController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
