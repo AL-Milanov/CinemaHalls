fbd53d059df1cb83f942d61ed317374462e54e93
AL-Milanov <alexandermilanov161195@gmail.com>
2022-11-07 18:00:56 +0200

    adding generic repository

---
+++ CinemaHalls.DataLayer/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaHalls.DataLayer.Repositories.Contracts
+{
+    public interface IRepository<T>
+    {
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(Guid entityId, T entity);
+        void DeleteAsync(T entity);
+        IQueryable<T> GetAllAsync();
+        Task SaveChangesAsync();
+    }
+}
---
+++ CinemaHalls.DataLayer/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using CinemaHalls.DataLayer.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaHalls.DataLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public void DeleteAsync(T entity)
+        {
+            DbSet<T>().Remove(entity);
+        }
+
+        public IQueryable<T> GetAllAsync()
+        {
+            return DbSet<T>();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> UpdateAsync(Guid entityId, T entity)
+        {
+            var entityToUpdate = await DbSet<T>().FindAsync(entityId);
+
+            if (entityToUpdate == null)
+            {
+                throw new Exception("Placeholder...");
+            }
+
+            return DbSet<T>().Update(entity).Entity;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return _dbContext.Set<T>();
+        }
+    }
+}
