6a0168f4b0156a9f8ae2fb08b7a1b4001918571c
AL-Milanov <alexandermilanov161195@gmail.com>
2022-11-08 17:38:40 +0200

    entity model

---
+++ CinemaHalls.DataLayer/Entities/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaHalls.DataLayer.Entities
+{
+    public class Genre
+    {
+        public Genre()
+        {
+            Movies = new HashSet<Movie>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ CinemaHalls.DataLayer/Entities/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaHalls.DataLayer.Entities
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            Genres = new HashSet<Genre>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Cast { get; set; }
+
+        public string Producer { get; set; }
+
+        public ushort Duration { get; set; }
+
+        public ICollection<Genre> Genres { get; set; }
+
+    }
+}
